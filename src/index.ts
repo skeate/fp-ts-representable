817672a73e82bfc0293ca50aba54a8c847ae3df2
Jonathan Skeate <skeate@gmail.com>
2022-09-30 20:36:50 -0400

    Fix build

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * as boundedGrid from './BoundedGrid'
+export * as distributive from './Distributive'
+export * as indexable from './Indexable'
+export * as key from './Key'
+export * as lookup from './Lookup'
+export * as representable from './Representable'
+export * as representableStore from './RepresentableStore'
